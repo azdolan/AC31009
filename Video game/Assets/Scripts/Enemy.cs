2831731e7e00fa9126aa3c91f30b4914f6fbc03a
azdolan <55203039+azdolan@users.noreply.github.com>
2022-04-13 18:37:44 +0100

    Attack things
    Fixed player attack script, created enemy script, created player attack left animation. Fixed some bugs

---
+++ Video game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void Damage(int enemyDamage)
+    {
+        currentHealth -= enemyDamage;
+
+        //play hurt animation
+
+        if(currentHealth <= 0)
+        {
+            enemyDie();
+        }
+    }
+
+    void enemyDie()
+    {
+        Debug.Log("Enemy died");
+        // die animation
+        animator.SetBool("IsDead", true);
+
+        // disable enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+    
+}
--- Video game/Assets/Scripts/PlayerAttack.cs
+++ Video game/Assets/Scripts/PlayerAttack.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public Animator animator;
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) // if the spacebar is pressed
         {
+
             Attack(); //calls the attack function
+
         }
     }
 
     void Attack()
     {
-        animator.SetTrigger("Attack");
+        animator.SetTrigger("Attack"); //plays the attack annimation 
+        Debug.Log("Attack");
 
+        Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in enemiesHit) // this loops for every item stored in the enemiesHit array
+        {
+            enemy.GetComponent<Enemy>().Damage(attackDamage);
+
+
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            if (attackPoint == null)
+            {
+                return;
+            }
+
+            // this draws a sphere on the editor which makes it easiser for me to visulaise how the player attacks the enemy
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
     }
 }
